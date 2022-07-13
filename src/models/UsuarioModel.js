fbd9a23547c9482c581e34afdd6499e642000cf0
Leonardo Resilia <leonardodacostadasilva@hotmail.com>
2022-07-13 19:31:44 -0300

    feat: criado model da entidade usuarios

---
+++ src/models/UsuarioModel.js
@@ -0,0 +1,9 @@
+class UsuarioModel{
+    constructor(nome, email, telefone){
+        this.nome = nome
+        this.email = email
+        this.telefone = telefone
+    }
+}
+
+export default UsuarioModel
