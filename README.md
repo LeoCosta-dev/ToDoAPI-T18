88b016b19cf7634d1d3831e1368c5d1573ca6564
Leonardo Resilia <leonardodacostadasilva@hotmail.com>
2022-07-20 19:01:29 -0300

    doc: Inicialização da documentação com os comandos de instalação e da rota post

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # ToDoAPI-T18
+
+## Como instalar:
+
+* Abra seu terminal em uma pasta de fácil acesso e cole o seguinte comando:
+
+```
+git clone https://github.com/LeoCosta-dev/ToDoAPI-T18.git
+```
+
+* Navegue até a pasta do projeto com o seguinte comando:
+
+```
+cd ToDoAPI-T18
+```
+
+* Instale as dependencias necessárias com o seguinte comando:
+
+```
+npm i
+```
+
+* Inicie o projeto rodando o comando:
+
+```
+npm start
+```
+
+## Exemplo de rotas:
+### Post
+url/usuarios
+```json
+{
+	"nome": "José",
+	"email": "couve@mineira.com.br",
+	"telefone": "2199999999"
+}
+```
+
+## Exemplo de resposta
+```json
+[
+	{
+		"nome": "José",
+		"email": "couve@mineira.com.br",
+		"telefone": "2199999999"
+	}
+]
+```
--- src/services/ValidacoesService.js
+++ src/services/ValidacoesService.js
@@ -1,13 +1,39 @@
 export default class ValidacoesService{
+    /**
+     * 
+     * @param {string} nome 
+     * @returns boolean
+     */
     static validaNome(nome){
         return nome.length >= 3
     }
+
+    /**
+     * Método que valida se o e-mail está no padrão "string@string.com"
+     * @param {string} email 
+     * @returns boolean
+     */
     static validaEmail(email){
         return true
     }
+
+    /**
+     * 
+     * @param {string} telefone 
+     * @returns boolean
+     */
     static validaTelefone(telefone){
-        return true
+        const tel = parseInt(telefone)
+        return tel == telefone
     }
+
+    /**
+     * 
+     * @param {string} nome 
+     * @param {string} email 
+     * @param {string} telefone 
+     * @returns boolean
+     */
     static isValid(nome, email, telefone){
         return this.validaNome(nome) && this.validaTelefone(telefone) && this.validaEmail(email)
     }
